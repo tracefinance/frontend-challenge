2c79614228f86413b6c4499cde7f7906916097d8
Felipe Oliveira <fr.oliveira94@gmail.com>
2022-06-06 19:03:15 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import { globalStyles } from '../styles/global';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles();
   return <Component {...pageProps} />
 }
 
