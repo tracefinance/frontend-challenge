28c0f7f9b5b9aae09a751d41a33c4a4592d5f404
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2022-06-11 17:20:43 -0400

    chore: self-host poppins font instead google fonts
    update: add font popping medium

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,10 @@
 import { globalCss } from '@stitches/react'
 
+import * as fonts from './fonts'
+
 export const globalStyles = globalCss({
+  // Spreading all the fonts into the global styles
+  '@font-face': [...Object.values(fonts)],
   '*': {
     margin: 0,
     padding: 0,
