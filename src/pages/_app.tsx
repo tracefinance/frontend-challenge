ede68495e50dda1eb8459155dc9d165e01fe39bd
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2022-06-11 16:50:50 -0400

    chore: setup eslint, prettier, absolute imports
    chore: add prettier config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import { globalStyles } from '../styles/global'
 import type { AppProps } from 'next/app'
+
+import { globalStyles } from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles()
