e7fb18a65bba944b2746d722147beb43e3ac6552
Gustavoctt <gustavocarrertartare@gmail.com>
2022-06-14 17:52:02 -0300

    Update Folder For Project Start

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+export default function Home(){
+  return (
+    <>
+      <Head>
+        <title>Cadastro | Teste técnico</title>
+      </Head>
+    </>
+  )
+}
