3debb50d0ca8e566370b94c45778733606f7261f
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2022-06-15 22:02:23 -0400

    fix: hardcode api url for demo purposes

--- src/services/fetch/fetchUser.ts
+++ src/services/fetch/fetchUser.ts
@@ -3,7 +3,11 @@ import { UserRegisterData } from '~/components/UserRegistrationForm/UserRegistra
 
 import { fetchJson } from './fetchJson'
 
-const url = `${process.env.NEXT_PUBLIC_API_URL}/user`
+// const url = `${process.env.NEXT_PUBLIC_API_URL}/user`
+/**
+ * Hardcoded url for the demo purposes
+ */
+const url = `https://629f52338b939d3dc29519e3.mockapi.io/api/challenge/user`
 
 /**
  * Make a POST to the API to register a new user
